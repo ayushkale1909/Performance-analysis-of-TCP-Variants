8028b3dbc7036491d2e52d140b84846976c748fd
ayush kale <74785780+ayushkale1909@users.noreply.github.com>
2023-08-30 21:44:21 -0400

    Update tcp-variants.py

--- Create TCP Reno and Vegas Simulation/tcp-variants.py
+++ Create TCP Reno and Vegas Simulation/tcp-variants.py
@@ -3,19 +3,16 @@ def analyze_trace_file(filename):
     received_packets = 0
     lost_packets = 0
     total_delay = 0.0
-    pkt_size = 0  # Initialize to default value
-    time = 0.0  # Initialize to default value
-    
+    pkt_size = 0  
+    time = 0.0  
     with open(filename, 'r') as f:
         for line in f:
             tokens = line.split()
             event_type = tokens[0]
             
-            # Update time and pkt_size only if the event_type is either "+" or "r"
             if event_type in ['+', 'r']:
-                time = float(tokens[1])  # Update time for each line
-                pkt_size = int(tokens[5])  # Update pkt_size for each line
-
+                time = float(tokens[1])  
+                pkt_size = int(tokens[5]) 
             from_node = tokens[2]
             to_node = tokens[3]
 
@@ -27,7 +24,6 @@ def analyze_trace_file(filename):
                 
     lost_packets = sent_packets - received_packets
 
-    # Metrics
     packet_loss_rate = (lost_packets / sent_packets) * 100 if sent_packets > 0 else 0
     throughput = (received_packets * pkt_size * 8) / time if time > 0 else 0  # in bits/sec
     average_delay = total_delay / received_packets if received_packets > 0 else 0  # in seconds
@@ -37,7 +33,6 @@ def analyze_trace_file(filename):
     print(f"Throughput: {throughput} bits/sec")
     print(f"Average End-to-End Delay: {average_delay} seconds")
 
-# Analyze the vegas.tr file
 analyze_trace_file("vegas.tr")
 
 # Analyze the tahoe.tr file
