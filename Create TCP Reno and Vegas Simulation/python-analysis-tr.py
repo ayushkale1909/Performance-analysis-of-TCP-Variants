6241d166906c9dfebeccc934668d903753505304
ayush kale <74785780+ayushkale1909@users.noreply.github.com>
2023-08-30 21:42:44 -0400

    Update python-analysis-tr.py

--- Create TCP Reno and Vegas Simulation/python-analysis-tr.py
+++ Create TCP Reno and Vegas Simulation/python-analysis-tr.py
@@ -14,17 +14,14 @@ def analyze_trace_file(filename):
             from_node = tokens[2]
             to_node = tokens[3]
 
-            # Count sent, received, and lost packets
             if event_type == "+":
                 sent_packets += 1
             elif event_type == "r":
                 received_packets += 1
-                total_delay += time  # This is a simplification; normally you'd match the sent and received packets
-            
-            # You may also add other conditions to filter by packet type, nodes, etc.
+                total_delay += time  
+                
     lost_packets = sent_packets - received_packets
 
-    # Calculate metrics
     packet_loss_rate = (lost_packets / sent_packets) * 100 if sent_packets > 0 else 0
     throughput = (received_packets * pkt_size * 8) / time  # in bits/sec
     average_delay = total_delay / received_packets if received_packets > 0 else 0  # in seconds
@@ -33,5 +30,4 @@ def analyze_trace_file(filename):
     print(f"Throughput: {throughput} bits/sec")
     print(f"Average End-to-End Delay: {average_delay} seconds")
 
-# Usage
 analyze_trace_file("tcp-example2.tr")
